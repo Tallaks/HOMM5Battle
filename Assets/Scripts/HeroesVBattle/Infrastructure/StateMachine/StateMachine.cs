3b4ed95718b11b888ed816b580ebd690c5dba7fe
Tallaks <sibirtsev@phystech.edu>
2021-11-11 02:54:00 +0300

    [003] Реализовал простейший вариант стейт-машины

---
+++ Assets/Scripts/HeroesVBattle/Infrastructure/StateMachine/StateMachine.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HeroesVBattle.Infrastructure.StateMachine
+{
+  public class StateMachine
+  {
+    private Dictionary<Type, IState> _states;
+    private IState _currentState;
+
+    public StateMachine()
+    {
+      _states = new Dictionary<Type, IState>
+      {
+
+      };
+    }
+
+    public void Enter<TState>(TState state) where TState : IState
+    {
+      _currentState.Exit();
+      _currentState = _states[typeof(TState)];
+      _currentState.Enter();
+    }
+  }
+}
