5907b95d8d8089cb5eae8807e229be0c8b666cf9
Tallaks <sibirtsev@phystech.edu>
2021-11-12 01:06:05 +0300

    [016] Первоначальный вид отображения дисклеймера при загрузке сцены

--- Assets/Scripts/HeroesVBattle/Infrastructure/StateMachine/LoadingLevelState.cs
+++ Assets/Scripts/HeroesVBattle/Infrastructure/StateMachine/LoadingLevelState.cs
@@ -1,11 +1,16 @@
+using UnityEngine;
+using Object = UnityEngine.Object;
+
 namespace HeroesVBattle.Infrastructure.StateMachine
 {
   public class LoadingLevelState : IState
   {
-    private const string Main = "Main";
-    
+    private const string MainSceneName = "Main";
+    private const string DisclaimerPrefabPath = "UI/Disclaimer";
+
     private readonly StateMachine _stateMachine;
     private readonly SceneLoader _sceneLoader;
+    private GameObject _disclaimerObject;
 
     public LoadingLevelState(StateMachine stateMachine, SceneLoader sceneLoader)
     {
@@ -13,14 +18,27 @@ namespace HeroesVBattle.Infrastructure.StateMachine
       _sceneLoader = sceneLoader;
     }
 
-    public void Enter() => 
+    public void Enter()
+    {
+      InstantiateDisclaimer();
       LoadMainScene();
+    }
 
     public void Exit()
     {
     }
 
+    private void InstantiateDisclaimer()
+    {
+      var disclaimerPrefab = Resources.Load<GameObject>(DisclaimerPrefabPath);
+      _disclaimerObject = Object.Instantiate(disclaimerPrefab);
+      Object.DontDestroyOnLoad(_disclaimerObject);
+    }
+
     private void LoadMainScene() => 
-      _sceneLoader.Load(Main);
+      _sceneLoader.Load(MainSceneName,OnLoadedScene);
+
+    private void OnLoadedScene() =>
+      Object.Destroy(_disclaimerObject);
   }
 }
