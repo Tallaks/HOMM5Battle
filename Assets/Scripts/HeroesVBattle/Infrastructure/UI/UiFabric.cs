7277c7dd883f333405d397790c87eccce85137d5
Tallaks <sibirtsev@phystech.edu>
2021-11-21 01:38:32 +0300

    [018] Сделал в фабрике возможность создавать HUD для режима рекогносцировки

--- Assets/Scripts/HeroesVBattle/Infrastructure/UI/UiFabric.cs
+++ Assets/Scripts/HeroesVBattle/Infrastructure/UI/UiFabric.cs
@@ -1,6 +1,24 @@
-﻿namespace HeroesVBattle.Infrastructure.UI
+﻿using HeroesVBattle.Infrastructure.StateMachine;
+using HeroesVBattle.Infrastructure.UI.Mediator;
+using UnityEngine;
+
+namespace HeroesVBattle.Infrastructure.UI
 {
   public class UiFabric
   {
+    private const string UIReconnaissancePrefabPath = "UI/UI - Reconnaissance";
+    
+    public UiGameplayMediator Create(IState gameState)
+    {
+      if (gameState.GetType() == typeof(ReconnaissanceState))
+        return InstantiateReconnaissanceHud();
+      else return null;
+    }
+
+    private UiGameplayMediator InstantiateReconnaissanceHud()
+    {
+      var reconnaissancePrefab = Resources.Load<GameObject>(UIReconnaissancePrefabPath);
+      return Object.Instantiate(reconnaissancePrefab).GetComponent<ReconnaissanceStateMediator>();
+    }
   }
 }
