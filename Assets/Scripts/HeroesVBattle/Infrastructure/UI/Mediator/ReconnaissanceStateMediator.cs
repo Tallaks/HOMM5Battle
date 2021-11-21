af7aa28797d4196a79d036eda73a5844eab28c7a
Tallaks <sibirtsev@phystech.edu>
2021-11-21 03:14:38 +0300

    [003] Инжекция стейт-машины в поле

--- Assets/Scripts/HeroesVBattle/Infrastructure/UI/Mediator/ReconnaissanceStateMediator.cs
+++ Assets/Scripts/HeroesVBattle/Infrastructure/UI/Mediator/ReconnaissanceStateMediator.cs
@@ -1,7 +1,13 @@
-﻿namespace HeroesVBattle.Infrastructure.UI.Mediator
+﻿using HeroesVBattle.Infrastructure.StateMachine;
+using Zenject;
+
+namespace HeroesVBattle.Infrastructure.UI.Mediator
 {
   public class ReconnaissanceStateMediator : UiGameplayMediator
   {
-    
+    [Inject]
+    private StateMachine.StateMachine _stateMachine;
+
+    public void StartBattle() => _stateMachine.Enter<EnemyPlacingState>();
   }
 }
