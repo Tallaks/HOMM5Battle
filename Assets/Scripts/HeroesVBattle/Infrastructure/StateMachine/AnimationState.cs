e31b0ec9158870126fc4859ff5bd48a7d009917b
Tallaks <sibirtsev@phystech.edu>
2021-11-23 17:05:06 +0300

    [003] Фаза анимации хода

---
+++ Assets/Scripts/HeroesVBattle/Infrastructure/StateMachine/AnimationState.cs
@@ -0,0 +1,18 @@
+﻿namespace HeroesVBattle.Infrastructure.StateMachine
+{
+  public class AnimationState : IState
+  {
+    private readonly StateMachine _stateMachine;
+
+    public AnimationState(StateMachine stateMachine) => 
+      _stateMachine = stateMachine;
+
+    public void Enter()
+    {
+    }
+
+    public void Exit()
+    {
+    }
+  }
+}
