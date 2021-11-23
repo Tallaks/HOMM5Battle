ea68b89c6e0eb9e616defa87c072b271bf960098
Tallaks <sibirtsev@phystech.edu>
2021-11-23 18:42:54 +0300

    [003] Контрольная панель для постоянных элементов

---
+++ Assets/Scripts/HeroesVBattle/UI/Common/CommonControlPanel.cs
@@ -0,0 +1,26 @@
+﻿using HeroesVBattle.Infrastructure.UI.Mediator;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace HeroesVBattle.UI.Common
+{
+  public class CommonControlPanel : MonoBehaviour
+  {
+    [SerializeField] private Button _surrenderButton;
+    private CommonMediator _mediator;
+    
+    private void Awake()
+    {
+      _surrenderButton.onClick.AddListener(Surrnder);
+      _mediator = GetComponent<CommonMediator>();
+    }
+
+    private void Surrnder() => 
+      _mediator.Surrneder();
+
+    private void OnDestroy()
+    {
+      _surrenderButton.onClick.RemoveAllListeners();
+    }
+  }
+}
