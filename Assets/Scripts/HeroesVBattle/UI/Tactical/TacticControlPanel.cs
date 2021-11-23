7773a1ab4f0f31feb9173023696d4c1f396c221c
Tallaks <sibirtsev@phystech.edu>
2021-11-23 17:47:47 +0300

    [021] Добавил обработку клика кнопки сдаться в контрольных панелях

--- Assets/Scripts/HeroesVBattle/UI/Tactical/TacticControlPanel.cs
+++ Assets/Scripts/HeroesVBattle/UI/Tactical/TacticControlPanel.cs
@@ -7,15 +7,27 @@ namespace HeroesVBattle.UI.Tactical
   public class TacticControlPanel : MonoBehaviour
   {
     [SerializeField] private Button _defenceButton;
+    [SerializeField] private Button _surrenderButton;
+
     private TacticalStateMediator _mediator;
 
     private void Awake()
     {
       _defenceButton.onClick.AddListener(Defend);
+      _surrenderButton.onClick.AddListener(Surrender);
       _mediator = GetComponent<TacticalStateMediator>();
     }
 
+    private void Surrender() => 
+      _mediator.Surrender();
+
     private void Defend() => 
       _mediator.Defend();
+
+    private void OnDestroy()
+    {
+      _defenceButton.onClick.RemoveAllListeners();
+      _surrenderButton.onClick.RemoveAllListeners();
+    }
   }
 }
