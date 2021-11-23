4f78a6d3acb309ae50a225cafeb4f18f50b219b2
Tallaks <sibirtsev@phystech.edu>
2021-11-23 16:48:29 +0300

    [020] Контрольная панель для тактической фазы

---
+++ Assets/Scripts/HeroesVBattle/UI/Tactical/TacticControlPanel.cs
@@ -0,0 +1,21 @@
+﻿using HeroesVBattle.Infrastructure.UI.Mediator;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace HeroesVBattle.UI.Tactical
+{
+  public class TacticControlPanel : MonoBehaviour
+  {
+    [SerializeField] private Button _defenceButton;
+    private TacticalStateMediator _mediator;
+
+    private void Awake()
+    {
+      _defenceButton.onClick.AddListener(Defend);
+      _mediator = GetComponent<TacticalStateMediator>();
+    }
+
+    private void Defend() => 
+      _mediator.Defend();
+  }
+}
