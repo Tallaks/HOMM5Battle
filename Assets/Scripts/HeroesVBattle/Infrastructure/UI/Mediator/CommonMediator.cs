293c736550e69caa94becbc4ab9bbf356364dee5
Tallaks <sibirtsev@phystech.edu>
2021-11-23 18:43:30 +0300

    [003] Медиатор для общих элементов

---
+++ Assets/Scripts/HeroesVBattle/Infrastructure/UI/Mediator/CommonMediator.cs
@@ -0,0 +1,13 @@
+﻿using HeroesVBattle.Infrastructure.StateMachine;
+using Zenject;
+
+namespace HeroesVBattle.Infrastructure.UI.Mediator
+{
+  public class CommonMediator : UiGameplayMediator
+  {
+    [Inject]
+    private StateMachine.StateMachine _stateMachine;
+
+    public void Surrneder() => _stateMachine.Enter<BattleResultState>();
+  }
+}
