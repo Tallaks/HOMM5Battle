807134f3ec43ea4c2a118b8f66d70837d3d63a67
Tallaks <sibirtsev@phystech.edu>
2021-11-27 03:21:01 +0300

    [008] Класс инициализационной даты с поддержкой версионности

---
+++ Assets/Scripts/HeroesVBattle/Data/GameData/InitialData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using HeroesVBattle.Data.EditorData;
+
+namespace HeroesVBattle.Data.GameData
+{
+  [Serializable]
+  public class InitialData
+  {
+    public Version Version;
+    public ArmyData PlayerArmy;
+    public ArmyData EnemyArmy;
+  }
+}
---
+++ Assets/Scripts/HeroesVBattle/Data/GameData/Version.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HeroesVBattle.Data.GameData
+{
+  [Serializable]
+  public struct Version
+  {
+    public int Main;
+    public int Additional;
+    public DateTime DateTime;
+
+    public override string ToString() => 
+      Main + "." + Additional;
+  }
+}
