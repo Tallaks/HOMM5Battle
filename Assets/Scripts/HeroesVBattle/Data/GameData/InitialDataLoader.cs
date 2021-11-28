3f6d5a3cc3cf453cd276fb032caaf6894d582a28
Tallaks <sibirtsev@phystech.edu>
2021-11-28 04:52:59 +0300

    [008] Загрузчик инициализационных данных

---
+++ Assets/Scripts/HeroesVBattle/Data/GameData/InitialDataLoader.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using HeroesVBattle.Data.ReferenceResolvers;
+using Sirenix.Serialization;
+using UnityEngine;
+
+namespace HeroesVBattle.Data.GameData
+{
+  public class InitialDataLoader
+  {
+    private string InitDataPath => Application.dataPath + "/Resources/Data/Initial/init.sav";
+
+    public InitialData LoadFromFile()
+    {
+      var context = new DeserializationContext()
+      {
+        StringReferenceResolver = new ScriptableObjectStringReferenceResolver(),
+      };
+
+      byte[] bytes = File.ReadAllBytes(InitDataPath);
+      return SerializationUtility.DeserializeValue<InitialData>(bytes,DataFormat.Binary,context);
+    }
+  }
+}
