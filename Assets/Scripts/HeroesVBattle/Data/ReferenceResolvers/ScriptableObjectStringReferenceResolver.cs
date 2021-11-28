fbc131f596f1d8969093527ac8c7b51b50343374
Tallaks <sibirtsev@phystech.edu>
2021-11-28 04:08:47 +0300

    [008] Рференс резолвер для сохранятора скриптабл обджектов

---
+++ Assets/Scripts/HeroesVBattle/Data/ReferenceResolvers/ScriptableObjectStringReferenceResolver.cs
@@ -0,0 +1,29 @@
+﻿using Sirenix.Serialization;
+using UnityEditor;
+using UnityEngine;
+
+namespace HeroesVBattle.Data.ReferenceResolvers
+{
+  public class ScriptableObjectStringReferenceResolver : IExternalStringReferenceResolver
+  {
+    public IExternalStringReferenceResolver NextResolver { get; set; } 
+
+    public bool CanReference(object value, out string id)
+    {
+      if (value is ScriptableObject)
+      {
+        id = AssetDatabase.AssetPathToGUID(AssetDatabase.GetAssetPath(value as ScriptableObject));
+        return true;
+      }
+
+      id = null;
+      return false;
+    }
+
+    public bool TryResolveReference(string id, out object value)
+    {
+      value = AssetDatabase.LoadAssetAtPath<ScriptableObject>(AssetDatabase.GUIDToAssetPath(id));
+      return value != null;
+    }
+  }
+}
