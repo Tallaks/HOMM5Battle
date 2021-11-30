840e02feb4ffb7e926bf86d37879abad71205411
Tallaks <sibirtsev@phystech.edu>
2021-11-30 23:19:46 +0300

    [032] Заготовок класса для героя специализации Mindreaver

---
+++ Assets/Scripts/HeroesVBattle/Gameplay/Units/Heroes/Inferno/MindReaver.cs
@@ -0,0 +1,11 @@
+﻿using HeroesVBattle.Data.EditorData;
+
+namespace HeroesVBattle.Gameplay.Units.Heroes.Inferno
+{
+  public sealed class MindReaver : Hero
+  {
+    public MindReaver(HeroData heroData) : base(heroData)
+    {
+    }
+  }
+}
