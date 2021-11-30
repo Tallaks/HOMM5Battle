d58e708f047f053869cc98119cca251ac870f252
Tallaks <sibirtsev@phystech.edu>
2021-11-30 23:18:41 +0300

    [032] Фабрика героев с учетом их специализации и фракции

---
+++ Assets/Scripts/HeroesVBattle/Gameplay/Units/Heroes/HeroFabric.cs
@@ -0,0 +1,44 @@
+﻿using HeroesVBattle.Data.EditorData;
+using HeroesVBattle.Gameplay.Units.Heroes.Haven;
+using HeroesVBattle.Gameplay.Units.Heroes.Inferno;
+
+namespace HeroesVBattle.Gameplay.Units.Heroes
+{
+  public class HeroFabric : IHeroFabric
+  {
+    public Hero Create(HeroData data)
+    {
+      switch (data.Faction)
+      {
+        case Faction.Haven:
+          return CreateHavenHero(data);
+        case Faction.Inferno:
+          return CreateInfernoHero(data);
+        default:
+          return null;
+      }
+    }
+
+    private Hero CreateInfernoHero(HeroData data)
+    {
+      switch (data.HeroType)
+      {
+        case "Mindreaver":
+          return new MindReaver(data);
+        default:
+          return null;
+      }
+    }
+
+    private Hero CreateHavenHero(HeroData data)
+    {
+      switch (data.HeroType)
+      {
+        case "Griffin Trainer":
+          return new GriffinTrainer(data);
+        default:
+          return null;
+      }
+    }
+  }
+}
---
+++ Assets/Scripts/HeroesVBattle/Gameplay/Units/Heroes/IHeroFabric.cs
@@ -0,0 +1,9 @@
+﻿using HeroesVBattle.Data.EditorData;
+
+namespace HeroesVBattle.Gameplay.Units.Heroes
+{
+  public interface IHeroFabric
+  {
+    Hero Create(HeroData data);
+  }
+}
