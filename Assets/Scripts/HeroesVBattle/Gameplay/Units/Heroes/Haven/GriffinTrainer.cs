a92b2c1c71d06804b6af7124c7aeeb9f75581307
Tallaks <sibirtsev@phystech.edu>
2021-11-30 23:19:13 +0300

    [032] Заготовок класса для специализации Griffin Trainer

---
+++ Assets/Scripts/HeroesVBattle/Gameplay/Units/Heroes/Haven/GriffinTrainer.cs
@@ -0,0 +1,11 @@
+﻿using HeroesVBattle.Data.EditorData;
+
+namespace HeroesVBattle.Gameplay.Units.Heroes.Haven
+{
+  public sealed class GriffinTrainer : Hero
+  {
+    public GriffinTrainer(HeroData heroData) : base(heroData)
+    {
+    }
+  }
+}
