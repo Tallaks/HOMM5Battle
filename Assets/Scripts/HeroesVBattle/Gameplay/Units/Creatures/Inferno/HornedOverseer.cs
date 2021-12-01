4847373fad2fabb749f7ac837e542e984187fbce
Tallaks <sibirtsev@phystech.edu>
2021-12-01 20:08:08 +0300

    [033] Заглушка для класса Horned Overseer

---
+++ Assets/Scripts/HeroesVBattle/Gameplay/Units/Creatures/Inferno/HornedOverseer.cs
@@ -0,0 +1,11 @@
+﻿using HeroesVBattle.Data.EditorData;
+
+namespace HeroesVBattle.Gameplay.Units.Creatures.Inferno
+{
+  public class HornedOverseer : Unit
+  {
+    public HornedOverseer(UnitData data) : base(data)
+    {
+    }
+  }
+}
