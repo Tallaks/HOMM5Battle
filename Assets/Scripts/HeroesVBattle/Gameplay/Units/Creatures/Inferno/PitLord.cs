028de03f4bb282d0253558f12d5daf144b64e0d7
Tallaks <sibirtsev@phystech.edu>
2021-12-01 20:08:34 +0300

    [033] Заглушка для класса Pit Lord

---
+++ Assets/Scripts/HeroesVBattle/Gameplay/Units/Creatures/Inferno/PitLord.cs
@@ -0,0 +1,11 @@
+﻿using HeroesVBattle.Data.EditorData;
+
+namespace HeroesVBattle.Gameplay.Units.Creatures.Inferno
+{
+  public class PitLord : Unit
+  {
+    public PitLord(UnitData data) : base(data)
+    {
+    }
+  }
+}
