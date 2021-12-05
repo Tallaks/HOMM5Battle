46e3e79ce2bca5b1bbb3b6d075ccf42a7a56d0fd
Tallaks <sibirtsev@phystech.edu>
2021-12-05 21:05:29 +0300

    [023] Имплементация интерфейсов абилок у Imperial Griffin

--- Assets/Scripts/HeroesVBattle/Gameplay/Units/Creatures/Haven/ImperialGriffin.cs
+++ Assets/Scripts/HeroesVBattle/Gameplay/Units/Creatures/Haven/ImperialGriffin.cs
@@ -1,10 +1,31 @@
 ﻿using HeroesVBattle.Data.EditorData;
+using HeroesVBattle.Gameplay.Units.Creatures.Abilities;
 
 namespace HeroesVBattle.Gameplay.Units.Creatures.Haven
 {
-  public class ImperialGriffin : Unit
+  public class ImperialGriffin : Unit, ILargeCreature, IUnlimitedRetaliation, IFlyer, IImmuneToBlind, IBattleDive
   {
     public ImperialGriffin(UnitData data) : base(data)
+    {
+    }
+
+    public void SetPositionForLarge()
+    {
+    }
+
+    public void Retaliate()
+    {
+    }
+
+    public void FlyOver()
+    {
+    }
+
+    public void IgnoreBlind()
+    {
+    }
+
+    public void MakeDive()
     {
     }
   }
