efe5cc13993874570fa5095fcc0b7612f3f2b921
Tallaks <sibirtsev@phystech.edu>
2021-12-05 16:52:52 +0300

    [034] Реализовал возможность сравнивать версии

--- Assets/Scripts/HeroesVBattle/Data/GameData/Version.cs
+++ Assets/Scripts/HeroesVBattle/Data/GameData/Version.cs
@@ -11,6 +11,27 @@ namespace HeroesVBattle.Data.GameData
     [HorizontalGroup(LabelWidth = 1)]
     public int Additional;
 
+    public static bool operator ==(Version a, Version b) => 
+      (a.Main == b.Main && a.Additional == b.Additional);
+
+    public static bool operator !=(Version a, Version b) => 
+      !(a == b);
+
+    public static bool operator >(Version a, Version b)
+    {
+      if (a.Main > b.Main) return true;
+      if (a.Main < b.Main) return false;
+      if (a.Main == b.Main)
+      {
+        if (a.Additional > b.Additional) return true;
+      }
+
+      return false;
+    }
+
+    public static bool operator <(Version a, Version b) => 
+      !(a > b);
+
     public override string ToString() => 
       Main + "." + Additional;
   }
