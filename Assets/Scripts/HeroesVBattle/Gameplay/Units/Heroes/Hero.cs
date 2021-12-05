66eacd4136742c4e0c1d82285b878e19a6b9d6f3
Tallaks <sibirtsev@phystech.edu>
2021-12-05 19:00:40 +0300

    [006] Кодбаза для абстрактного героя

--- Assets/Scripts/HeroesVBattle/Gameplay/Units/Heroes/Hero.cs
+++ Assets/Scripts/HeroesVBattle/Gameplay/Units/Heroes/Hero.cs
@@ -1,11 +1,31 @@
 ﻿using HeroesVBattle.Data.EditorData;
+using UnityEngine;
 
 namespace HeroesVBattle.Gameplay.Units.Heroes
 {
   public abstract class Hero
   {
+    protected readonly Texture Icon; 
+    
+    protected readonly Faction Faction;
+
+    protected readonly int Attack;
+    protected readonly int Defence;
+    protected readonly int SpellPower;
+    protected readonly int Knowledge;
+    protected readonly int Luck;
+    protected readonly int Morale;
+
     public Hero(HeroData heroData)
     {
+      Attack = heroData.Attack;
+      Defence = heroData.Defence;
+      SpellPower = heroData.SpellPower;
+      Knowledge = heroData.Knowledge;
+      Luck = heroData.Luck;
+      Morale = heroData.Morale;
+      Faction = heroData.Faction;
+      Icon = heroData.Icon;
     }
   }
 }
