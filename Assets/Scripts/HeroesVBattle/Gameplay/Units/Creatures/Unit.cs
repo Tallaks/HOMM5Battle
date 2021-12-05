2d17416acf496b921cc368005b20be1723dc0a55
Tallaks <sibirtsev@phystech.edu>
2021-12-05 20:19:35 +0300

    Кодбаза для абстрактного юнита

--- Assets/Scripts/HeroesVBattle/Gameplay/Units/Creatures/Unit.cs
+++ Assets/Scripts/HeroesVBattle/Gameplay/Units/Creatures/Unit.cs
@@ -1,11 +1,17 @@
 ﻿using HeroesVBattle.Data.EditorData;
+using UnityEngine;
 
 namespace HeroesVBattle.Gameplay.Units.Creatures
 {
   public abstract class Unit
   {
+    public Sprite Icon { get; }
+    public int Quantity { get; set; }
+
     public Unit(UnitData unitData)
     {
+      Quantity = unitData.Quantity;
+      Icon = unitData.Icon;
     }
   }
 }
