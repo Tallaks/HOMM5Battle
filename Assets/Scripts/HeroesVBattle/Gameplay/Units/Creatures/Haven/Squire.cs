edcf5bd8ddfa5a7f5222e822d741abd5df8622a8
Tallaks <sibirtsev@phystech.edu>
2021-12-05 21:12:55 +0300

    [005] Имплементация интерфейсов абилок для Squire

--- Assets/Scripts/HeroesVBattle/Gameplay/Units/Creatures/Haven/Squire.cs
+++ Assets/Scripts/HeroesVBattle/Gameplay/Units/Creatures/Haven/Squire.cs
@@ -1,10 +1,27 @@
 ﻿using HeroesVBattle.Data.EditorData;
+using HeroesVBattle.Gameplay.Units.Creatures.Abilities;
 
 namespace HeroesVBattle.Gameplay.Units.Creatures.Haven
 {
-  public class Squire : Unit
+  public class Squire : Unit, ILargeShield, IShieldAllies, IBash, IEnraged
   {
     public Squire(UnitData data) : base(data)
+    {
+    }
+
+    public void ProtectFromShot()
+    {
+    }
+
+    public void ProtectAllies()
+    {
+    }
+
+    public void Bash()
+    {
+    }
+
+    public void IncreaseAttack()
     {
     }
   }
