c28caf6fa93bf53d7c0694be5ca56bf810a5bb82
Tallaks <sibirtsev@phystech.edu>
2021-12-09 02:30:32 +0300

    [024] Кодабаза для ImperialGriffin, нужная для данной версии игры

--- Assets/Scripts/HeroesVBattle/Gameplay/Units/Heroes/Haven/GriffinTrainer.cs
+++ Assets/Scripts/HeroesVBattle/Gameplay/Units/Heroes/Haven/GriffinTrainer.cs
@@ -1,11 +1,26 @@
 ﻿using HeroesVBattle.Data.EditorData;
+using HeroesVBattle.Gameplay.Units.Creatures;
+using HeroesVBattle.Gameplay.Units.Creatures.Haven;
 
 namespace HeroesVBattle.Gameplay.Units.Heroes.Haven
 {
   public sealed class GriffinTrainer : Hero
   {
-    public GriffinTrainer(HeroData heroData) : base(heroData)
+    public GriffinTrainer(HeroData heroData, Army army) : base(heroData, army)
     {
+    }
+
+    public override void UseSpecialization()
+    {
+      foreach (Unit unit in Army.Units)
+      {
+        if (unit.GetType() == typeof(ImperialGriffin))
+        {
+          int modificator = Level / 2;
+          unit.Attack += modificator;
+          unit.Defence += modificator;
+        }
+      }
     }
   }
 }
