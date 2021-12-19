245692906e00ce55a9ff50e922c051394e0fd354
Tallaks <sibirtsev@phystech.edu>
2021-12-19 05:29:27 +0300

    Операция выгрузки сцены в загрузчике сцены

--- Assets/Scripts/HeroesVBattle/Infrastructure/SceneLoader.cs
+++ Assets/Scripts/HeroesVBattle/Infrastructure/SceneLoader.cs
@@ -13,12 +13,28 @@ namespace HeroesVBattle.Infrastructure
     
     public void Load(string sceneName, Action onLoaded = null) => 
       _coroutineRunner.StartCoroutine(LoadScene(sceneName, onLoaded));
+    
+    public void LoadAdditive(string sceneName, Action onLoaded = null) => 
+      _coroutineRunner.StartCoroutine(LoadScene(sceneName, onLoaded, true));
 
-    private IEnumerator LoadScene(string sceneName, Action onLoaded = null)
+    public void Unload(string sceneName) =>
+      _coroutineRunner.StartCoroutine(UnloadScene(sceneName));
+
+    private IEnumerator UnloadScene(string sceneName)
+    {
+      yield return null;
+
+      AsyncOperation unloadOperation = SceneManager.UnloadSceneAsync(sceneName);
+      while (!unloadOperation.isDone)
+        yield return null;
+    }
+
+    private IEnumerator LoadScene(string sceneName, Action onLoaded = null, bool additive = false)
     {
       yield return null;
       
-      AsyncOperation loadOperation = SceneManager.LoadSceneAsync(sceneName);
+      AsyncOperation loadOperation =
+        SceneManager.LoadSceneAsync(sceneName, additive == true ? LoadSceneMode.Additive : LoadSceneMode.Single);
       while (!loadOperation.isDone)
         yield return null;
 
