75f837505fd9efd797eafb3d2402611ad872b559
Tallaks <sibirtsev@phystech.edu>
2021-12-19 05:26:43 +0300

    [045] Медиатор для UI инфопанели героя и кнопка возврата к игре

---
+++ Assets/Scripts/HeroesVBattle/Infrastructure/UI/Mediator/HeroInfoMediator.cs
@@ -0,0 +1,28 @@
+﻿using HeroesVBattle.Gameplay.Units;
+using HeroesVBattle.Infrastructure.StateMachine;
+using HeroesVBattle.UI.HeroInfo;
+using UnityEngine;
+using Zenject;
+
+namespace HeroesVBattle.Infrastructure.UI.Mediator
+{
+  public class HeroInfoMediator : UiGameplayMediator
+  {
+    [Inject]
+    private StateMachine.StateMachine _stateMachine;
+
+    [SerializeField] 
+    private HeroInfoPanel _infoPanel;
+
+    [SerializeField] 
+    private ReturnButton _returnButton;
+
+    private Army _army;
+
+    public void InitArmy(Army army) => _army = army;
+    public void InitInfoPanel() => _infoPanel.Init(_army);
+    public void InitBackButton() => _returnButton.Init(_army);
+    public void ReturnToReconaissanse() => _stateMachine.EnterWithParameter<ReconnaissanceState, Army>(_army);
+    public void ReturnToBattle() => _stateMachine.EnterWithParameter<TacticalState, Army>(_army);
+  }
+}
---
+++ Assets/Scripts/HeroesVBattle/UI/HeroInfo/ReturnButton.cs
@@ -0,0 +1,32 @@
+﻿using HeroesVBattle.Gameplay.Units;
+using HeroesVBattle.Infrastructure.UI.Mediator;
+using HeroesVBattle.UI.Interaction;
+using UnityEngine;
+
+namespace HeroesVBattle.UI.HeroInfo
+{
+  public class ReturnButton : MonoBehaviour
+  {
+    [SerializeField] 
+    private UiButton _button;
+
+    [SerializeField] 
+    private HeroInfoMediator _mediator;
+
+    private Army _army;
+
+    private void Awake() => 
+      _button.OnClick.AddListener(ReturnToGame);
+
+    public void Init(Army army) => 
+      _army = army;
+
+    private void ReturnToGame()
+    {
+      if (_army.IsPlaced)
+        _mediator.ReturnToBattle();
+      else
+        _mediator.ReturnToReconaissanse();
+    }
+  }
+}
