f1808427db706878f6ad11ce47d77a0e7f5c82f6
Tallaks <sibirtsev@phystech.edu>
2021-12-19 05:28:46 +0300

    [045] Добавление кнопки на иконку героя для перехода к стейту информации о герое

--- Assets/Scripts/HeroesVBattle/UI/Reconnaissance/ArmyPanel.cs
+++ Assets/Scripts/HeroesVBattle/UI/Reconnaissance/ArmyPanel.cs
@@ -1,5 +1,7 @@
 using System;
 using HeroesVBattle.Gameplay.Units;
+using HeroesVBattle.Infrastructure.UI.Mediator;
+using HeroesVBattle.UI.Interaction;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,11 +10,31 @@ namespace HeroesVBattle.UI.Reconnaissance
   public class ArmyPanel : MonoBehaviour
   {
     private const int ArmySize = 7;
-    
+
+    [Header("Mediator")] 
+    [SerializeField] private ReconnaissanceStateMediator _mediator;
+
+    [Header("Icons")]
     [SerializeField] private Image _heroIcon;
     [SerializeField] private SquadElement[] _unitElement = new SquadElement[ArmySize];
 
-    public void InitIcons(Army army)
+    [Space]
+    [Header("Buttons")]
+    [SerializeField] private UiButton _heroIconButton;
+
+    public void Init(Army army)
+    {
+      InitIcons(army);
+      InitButtons(army);
+    }
+
+    public void Hide() => 
+      gameObject.SetActive(false);
+
+    private void InitButtons(Army army) => 
+      _heroIconButton.OnDoubleClick.AddListener(() => _mediator.OpenHeroInfo(army));
+
+    private void InitIcons(Army army)
     {
       _heroIcon.sprite = army.Hero.Icon;
       
