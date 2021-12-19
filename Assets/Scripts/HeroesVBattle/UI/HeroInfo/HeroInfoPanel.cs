b800d636850ec341ee73e2c438077f3dcdf7cf98
Tallaks <sibirtsev@phystech.edu>
2021-12-19 05:23:22 +0300

    Заглушка для общего класса панели с инфой по герою

---
+++ Assets/Scripts/HeroesVBattle/UI/HeroInfo/HeroInfoPanel.cs
@@ -0,0 +1,13 @@
+﻿using HeroesVBattle.Gameplay.Units;
+using UnityEngine;
+
+namespace HeroesVBattle.UI.HeroInfo
+{
+  public class HeroInfoPanel : MonoBehaviour
+  {
+    public void Init(Army army)
+    { 
+      
+    }
+  }
+}
